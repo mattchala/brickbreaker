8ad6c6032b4caeed872edd889dfdef149eb82dd5
Hassan <hchaudhry17@gmail.com>
2023-07-12 02:20:10 -0400

    Removed boolean check for scene name and instead use GetActiveScene method

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,6 @@ public class GameManager : MonoBehaviour
     public int lives = 3;
     public int level = 1;
 
-    public bool isGlobal = true;
-
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);     //  this will persist across loaded and deleted scenes
@@ -17,7 +15,10 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        if (isGlobal) 
+        // HASSAN: Load up main menu if current scene is Global
+        Scene loadedScene = SceneManager.GetActiveScene();
+        string scene = loadedScene.name;
+        if (scene == "Global") 
         {
             SceneManager.LoadScene("Menu");
         }
@@ -26,7 +27,7 @@ public class GameManager : MonoBehaviour
     // HASSAN: Loads up Level_1 when the play button is pressed on main menu.
     public void OnClickPlay() 
     {
-        Destroy(gameObject);  // Overrides DontDestroyOnLoad to get rid of main menu on level load
+        Destroy(gameObject);  // HASSAN: Overrides DontDestroyOnLoad to get rid of main menu on level load
         NewGame();
     }
 
@@ -47,7 +48,7 @@ public class GameManager : MonoBehaviour
     // HASSAN: Game app quits when exit button is pressed on main menu
     public void OnClickExit() 
     {
-        Debug.Log("EXIT");  // Only for debug purposes to verify exit functionality in editor
+        Debug.Log("EXIT");  // HASSAN: Only for debug purposes to verify exit functionality in editor
         Application.Quit();
     }
 
