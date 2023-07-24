87fc42de7db4978930458cf1985ba5a4e1e6b076
Hassan <hchaudhry17@gmail.com>
2023-07-24 16:53:47 -0400

    Added player name input for high score tables

---
+++ Assets/Scripts/PlayerNameInput.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    
+    public string playerName;
+    public GameObject inputFieldPanel;
+    public TMP_InputField inputField;
+
+    public void OnClickEnter()
+    {
+        playerName = inputField.text;
+        inputFieldPanel.SetActive(false);
+        if (playerName == "")
+        {
+            PlayerPrefs.SetString("PlayerName", "???");
+        }
+        else
+        {
+            PlayerPrefs.SetString("PlayerName", playerName);
+        }
+        PlayerPrefs.Save();
+        Debug.Log(PlayerPrefs.GetString("PlayerName"));
+    }
+
+}
