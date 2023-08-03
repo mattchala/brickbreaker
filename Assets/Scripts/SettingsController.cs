59b77dc5bf7be7eeae8e5963bc2b384ffb2e4dcf
Matt Chalabian <70189609+mattchala@users.noreply.github.com>
2023-08-03 00:23:53 -0700

    Added functioning particle setting

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -11,6 +11,9 @@ public class SettingsController : MonoBehaviour
     [SerializeField] private Slider animationsSlider = null;
     [SerializeField] private Text animationsTextUI = null;
 
+    [SerializeField] private Slider particlesSlider = null;
+    [SerializeField] private Text particlesTextUI = null;
+
     [SerializeField] private Slider screenShakeSlider = null;
     [SerializeField] private Text screenShakeTextUI = null;
 
@@ -27,9 +30,11 @@ public class SettingsController : MonoBehaviour
         float musicVolumeValue = PlayerPrefs.GetFloat("MusicVolume");
         float animationsOn = PlayerPrefs.GetFloat("AnimationsOn");
         float screenShakeOn = PlayerPrefs.GetFloat("ScreenShakeOn");
+        float particlesOn = PlayerPrefs.GetFloat("ParticlesOn");
         float playerDifficulty = PlayerPrefs.GetFloat("PlayerDifficulty");
         
         musicVolumeSlider.value = musicVolumeValue;
+        particlesSlider.value = particlesOn;
         animationsSlider.value = animationsOn;
         screenShakeSlider.value = screenShakeOn;
         playerDifficultySlider.value = playerDifficulty;
@@ -42,6 +47,9 @@ public class SettingsController : MonoBehaviour
 
         float animations = animationsSlider.value;
         PlayerPrefs.SetFloat("AnimationsOn", animations);
+
+        float particles = particlesSlider.value;
+        PlayerPrefs.SetFloat("ParticlesOn", particles);
 
         float screenShake = screenShakeSlider.value;
         PlayerPrefs.SetFloat("ScreenShakeOn", screenShake);
@@ -60,6 +68,11 @@ public class SettingsController : MonoBehaviour
     public void AnimationsSlider(float animations)
     {
         animationsTextUI.text = animations == 1.0 ? "On" : "Off";
+    }
+
+    public void ParticlesSlider(float particles)
+    {
+        particlesTextUI.text = particles == 1.0 ? "On" : "Off";
     }
 
     public void ScreenShakeSlider(float screenShake)
